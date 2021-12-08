371716c98a00b10b608fc61ccd12dc01517796f9
arthurhtml <arthur.dbs70@gmail.com>
2021-12-08 17:46:56 +0100

    partie gauche finis

---
+++ mouseButton.c
@@ -0,0 +1,8 @@
+//
+//  mouseButton.c
+//  Cmusic
+//
+//  Created by Arthur dubois on 08/12/2021.
+//
+
+#include "mouseButton.h"
---
+++ mouseButton.h
@@ -0,0 +1,13 @@
+//
+//  mouseButton.h
+//  Cmusic
+//
+//  Created by Arthur dubois on 08/12/2021.
+//
+
+#ifndef mouseButton_h
+#define mouseButton_h
+
+#include <stdio.h>
+
+#endif /* mouseButton_h */
