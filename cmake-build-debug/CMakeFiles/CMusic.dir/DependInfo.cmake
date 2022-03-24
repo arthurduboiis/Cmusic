fefc1f1a581a4b2939446f338ec1290d89f8dc5f
Antoine VINE <antoine.vine78@gmail.com>
2022-03-24 15:22:35 +0100

    Merge in progress

---
+++ cmake-build-debug/CMakeFiles/CMusic.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/dropEvent.c" "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/cmake-build-debug/CMakeFiles/CMusic.dir/dropEvent.c.obj"
+  "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/initMedia.c" "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/cmake-build-debug/CMakeFiles/CMusic.dir/initMedia.c.obj"
+  "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/main.c" "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/cmake-build-debug/CMakeFiles/CMusic.dir/main.c.obj"
+  "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/mouseButton.c" "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/cmake-build-debug/CMakeFiles/CMusic.dir/mouseButton.c.obj"
+  "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/render.c" "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/cmake-build-debug/CMakeFiles/CMusic.dir/render.c.obj"
+  "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/viewport.c" "C:/Users/toino/Desktop/IUT/S3/Ptut/CMusic/cmake-build-debug/CMakeFiles/CMusic.dir/viewport.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CMusic.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMusic.dir/dropEvent.c.obj"
+  "CMakeFiles/CMusic.dir/initMedia.c.obj"
+  "CMakeFiles/CMusic.dir/main.c.obj"
+  "CMakeFiles/CMusic.dir/mouseButton.c.obj"
+  "CMakeFiles/CMusic.dir/render.c.obj"
+  "CMakeFiles/CMusic.dir/viewport.c.obj"
+  "CMusic.exe"
+  "CMusic.exe.manifest"
+  "CMusic.pdb"
+  "libCMusic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CMusic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
